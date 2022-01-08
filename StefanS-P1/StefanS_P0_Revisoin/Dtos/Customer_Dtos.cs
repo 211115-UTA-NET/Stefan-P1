ab2543911b9e5f8219d15d3697b773f6163bf582
Stefan Schweers <schweers.stefan@yahoo.com>
2022-01-08 16:04:51 -0500

    completted SQL to API to Console connections on testing

--- StefanS-P1/StefanS_P0_Revisoin/Dtos/Customer_Dtos.cs
+++
@@ -1,9 +0,0 @@
-namespace Dtos
-{
-    public class Customer_Dtos
-    {
-        public int id { get; set; }
-        public string? user { get; set; }
-        public int seshId { get; set; }
-    }
-}
--- StefanS-P1/StefanS_P0_Revisoin/Dtos/ShoppingCart_Dtos.cs
+++
@@ -1,9 +0,0 @@
-namespace Dtos
-{
-    public class ShoppingCart_Dtos
-    {
-        string? item { get; set;}
-        decimal price { get; set;}
-        int quantity { get; set;}
-    }
-}
